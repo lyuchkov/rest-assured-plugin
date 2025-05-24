4ea72df7f1e9dd1882eacc294fdcef2a9fa0c68d
GitHub Action <action@github.com>
2025-05-24 14:38:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/lyuchkov/restassuredplugin/MyBundle.kt
+++ src/main/kotlin/com/github/lyuchkov/restassuredplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.lyuchkov.restassuredplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/lyuchkov/restassuredplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/lyuchkov/restassuredplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.lyuchkov.restassuredplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.lyuchkov.restassuredplugin.MyBundle
+import com.github.lyuchkov.restassuredplugin.services.MyProjectService
 import javax.swing.JButton
 
 
